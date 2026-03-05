aa007f525aa0a758bd4f953550f9a5065f4762a6
Arkadiusz Szczęnsy <szczesny.arkadiusz96@gmail.com>
2026-03-05 14:56:25 +0100

    init: project setup

---
+++ src/main/java/com/github/arkadiuszszczesny/escaperoomtracker/EscapeRoomTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.github.arkadiuszszczesny.escaperoomtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscapeRoomTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/arkadiuszszczesny/escaperoomtracker/EscapeRoomTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arkadiuszszczesny.escaperoomtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
